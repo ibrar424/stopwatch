4c3522f2873c1b31a04751edec476ad8fb5bec10
ibrar ul hasan <81883703+ibrar424@users.noreply.github.com>
2025-10-13 18:10:21 +0500

    Delete countdown.py

--- countdown.py
+++
@@ -1,9 +0,0 @@
-import time
-my_time = int(input("Enter time in second"))
-for i in range(my_time, 0, -1):
-    Second = i % 60
-    minutes =int(i / 60) % 60
-    hours = int(i / 3600)
-
-    print(f"{hours:02}:{minutes:02}:{Second:02}")
-    time.sleep(1)
